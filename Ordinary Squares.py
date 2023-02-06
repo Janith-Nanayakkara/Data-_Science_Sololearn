72ec201b31b1c0e21e5168778156a69be718c6b6
W.Janith Induwara Nanayakkara <76847028+Janith-Nanayakkara@users.noreply.github.com>
2023-02-06 11:15:38 +0530

    Ordinary Squares

---
+++ Ordinary Squares.py
@@ -0,0 +1,14 @@
+n, p = [int(x) for x in input().split()]
+X = []
+for i in range(n):
+    X.append([float(x) for x in input().split()])
+
+y = [float(x) for x in input().split()]
+
+
+import numpy as np
+
+X=np.array(X).reshape(n,p)
+y=np.array(y)
+b=np.linalg.pinv(X) @ y.transpose() 
+print(np.around(b,decimals=2))
